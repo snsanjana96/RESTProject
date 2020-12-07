fc2413abd3199dc28b1026f321152940b3f8dd0b
snsanjana96 <snsanjana7@gmail.com>
2020-12-07 14:30:57 +0530

    RESTProject

---
+++ src/main/java/SDETBatch/App.java
@@ -0,0 +1,13 @@
+package SDETBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
